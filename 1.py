06a8203b44a68e2a1ea89d63d219b1d7ca7ce1ec
Akash <akashmahajan0898@gmail.com>
2021-10-06 17:05:41 +0530

    py file added

---
+++ 1.py
@@ -0,0 +1,28 @@
+test = 'Hello'
+test1 = 'World'
+print(test + " " + test1)
+
+test2 = 10
+test3 = 20
+print(test2 * test3)
+
+test4 = 1.5
+test5 = 1.5
+print(test4 * test5)
+
+print(type(test4))
+print(type(test2))
+
+x = 'abcd xyz'
+print(type(x))
+
+a = True
+b = False
+print(type(a))
+
+c = [1, 2, 3, 4]
+print(type(c))
+print(c)
+
+f = input(int)
+print(f)
